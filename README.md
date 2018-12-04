41166fe5051dffac7a8edd242d396c016f59809a
tomburgoyne <tomburgoyne@live.co.uk>
2018-12-04 21:45:12 +0000

    Bumped package numbers and added some addition test examples.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # mocha_http_test_framework
-Basic HTTP assertion testing using Mocha and SuperTest.
+Basic HTTP assertion testing using Mocha and SuperTest with various examples of how it can be used.
--- test/test.js
+++ test/test.js
@@ -1,14 +1,45 @@
 'use strict';
 
-const request = require('supertest')("https://www.google.co.uk/");
+const request = require('supertest')("https://www.google.co.uk");
 
-describe('Your API test', function() {
-    it('should return a 200 status code', function(done) {
-      request.get("")
-        .expect(200)
-        .end(function(err, res) {
+describe('Your API test', function () {
+  it('should return a 200 status code', function (done) {
+    request.get('/')
+      .expect(200)
+      .end(function (err) {
         if (err) return done(err);
         done();
       });
-    });
+  });
+
+  it('should check the response body is not null', function (done) {
+    request.get('/')
+      .expect(checkResponseBody)
+      .end(function (err) {
+        if (err) return done(err);
+        done();
+      });
+
+    function checkResponseBody(res) {
+      if (res.body === null) {
+        throw new Error("Response body was null");
+      }
+    }
+  });
+
+  it('can check a status code and something else in one test', function (done) {
+    request.get('/')
+      .expect(200)
+      .expect(checkSomethingElse)
+      .end(function (err) {
+        if (err) return done(err);
+        done();
+      });
+
+    function checkSomethingElse(res) {
+      if (res.status === 200) {
+        throw new Error("Custom error message for status code not being as expected")
+      }
+    }
+  });
 });
